8a6fad283e753b232a922d08e1077fcaa95a7b6d
kzfmhpvomo@gmail.com <kzfmhpvomo@gmail.com>
2021-01-14 17:21:08 +0900

    👍

---
+++ 7Origin.bat
@@ -0,0 +1,4 @@
+@echo off
+setlocal enabledelayedexpansion
+cd C:\Program Files (x86)\Origin Games\Apex\media
+del /p  respawn.bik
---
+++ 7Steam.bat
@@ -0,0 +1,4 @@
+@echo off
+setlocal enabledelayedexpansion
+cd C:\Program Files (x86)\Steam\steamapps\common\Apex Legends\media
+del /p  respawn.bik
